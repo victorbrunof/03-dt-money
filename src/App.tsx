61f8a5450c2ef598031f8f0e6a449c6cfcc16226
Victor Bruno <victorbrunof@icloud.com>
2022-10-17 21:00:36 -0300

    Estrutura visual

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/theme/default";
+
 export function App() {
   return (
-    <h1>Hello world</h1>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Transactions />
+    </ThemeProvider>
+  );
 }
-
