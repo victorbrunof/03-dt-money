fb87a4de93db770a83ac223677ac2cf10c1b42b8
Victor Bruno <victorbrunof@icloud.com>
2022-10-18 21:04:02 -0300

    Estrutura visual - Criando radio button acessível

--- src/components/newTransactionModal/index.tsx
+++ src/components/newTransactionModal/index.tsx
@@ -26,12 +26,12 @@ export function NewTransationModal() {
           <input type="text" placeholder="Categoria" required />
 
           <TransactionType>
-            <TransactionTypeButton variant="income">
+            <TransactionTypeButton variant="income" value="income">
               <ArrowCircleUp size={24} />
               Entrada
             </TransactionTypeButton>
 
-            <TransactionTypeButton variant="outcome">
+            <TransactionTypeButton variant="outcome" value="outcome">
               <ArrowCircleDown size={24} />
               Saida
             </TransactionTypeButton>
